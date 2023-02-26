aac21375ccc2bd4c9ece4d38052614aa21e2b20f
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 20:04:56 +0700

    Course-Angular

---
+++ src/app/joinnow/joinnow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joinnow',
+  templateUrl: './joinnow.component.html',
+  styleUrls: ['./joinnow.component.css']
+})
+export class JoinnowComponent {
+
+}
